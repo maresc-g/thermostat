76e5d02e0d03c2fef1b90f83064bad5c941add9d
Guillaume Marescaux <guillaume.marescaux@gmail.com>
2022-03-22 21:09:12 +0100

    Using async sleep

--- server/src/exec/mod.rs
+++ server/src/exec/mod.rs
@@ -1,4 +1,5 @@
-use std::{thread, time};
+use std::{time};
+use tokio::time::{Duration};
 use crate::db::temperature;
 use super::db;
 
@@ -6,15 +7,16 @@ const TEMPERATURE_CHECK_DT: u128 = 5000;
 
 pub async fn run_main_loop()  {
     let mut last_temp_check = time::UNIX_EPOCH;
+    let mut interval = tokio::time::interval(Duration::from_secs(1));
     let db = db::DbItf::new().await;
+    interval.tick().await;
     loop {
+        println!("Checking if state needs to change");
         if last_temp_check.elapsed().unwrap().as_millis() > TEMPERATURE_CHECK_DT {
             let temperature = 20_f64;
             temperature::insert(&db, temperature).await;
             last_temp_check = time::SystemTime::now();
         }
-        println!("Checking if state needs to change");
-        let sec = time::Duration::from_secs(1);
-        thread::sleep(sec);
+        interval.tick().await;
     }
 }
