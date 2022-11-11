5288624bb30673570984843ce93d6b127911da2e
Guillaume Marescaux <guillaume.marescaux@gmail.com>
2022-11-11 22:28:18 +0100

    Upgraded to latest version of vuejs

--- webapp/src/router/index.ts
+++ webapp/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
@@ -18,7 +18,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
