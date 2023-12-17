681bed7cfbc932461eb4f1119df9db2e01a4db95
Guillaume Marescaux <guillaume.marescaux@gmail.com>
2023-12-17 11:05:02 +0100

    test i2c

--- server/src/hal/thermometer/mod.rs
+++ server/src/hal/thermometer/mod.rs
@@ -1,3 +1,3 @@
-pub mod ti;
+pub mod ada;
 pub mod simu;
 
--- server/src/main.rs
+++ server/src/main.rs
@@ -6,17 +6,38 @@ mod hal;
 #[macro_use]
 extern crate log;
 
+extern crate i2cdev;
+use std::thread;
+use std::time::Duration;
+
+use i2cdev::core::*;
+use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
+
+const ADA_SLAVE_ADDR: u16 = 0x18;
+const ADA_REG_TEMP: u8 = 0x5;
+
 #[tokio::main]
 async fn main() {
     env_logger::init();
     info!("starting up");
 
-    let handle_main_loop = tokio::spawn(async {
-        exec::run_main_loop().await;
-    });
-    let handle_http_server = tokio::spawn(async {
-        api::run_http_server().await;
-    });
-    handle_main_loop.await.unwrap();
-    handle_http_server.await.unwrap();
+    let mut dev = LinuxI2CDevice::new("/dev/i2c-1", ADA_SLAVE_ADDR).unwrap();
+
+    // init sequence
+    loop {
+        let mut buf: [u8; 2] = [0; 2];
+        dev.smbus_write_byte(ADA_REG_TEMP).unwrap();
+        dev.read(&mut buf).unwrap();
+        println!("Reading: {:?}", buf);
+        thread::sleep(Duration::from_millis(200));
+    }
+
+//     let handle_main_loop = tokio::spawn(async {
+//         exec::run_main_loop().await;
+//     });
+//     let handle_http_server = tokio::spawn(async {
+//         api::run_http_server().await;
+//     });
+//     handle_main_loop.await.unwrap();
+//     handle_http_server.await.unwrap();
 }
